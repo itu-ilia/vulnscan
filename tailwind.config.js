692de88f898765fd2556c02b398392b4d039379d
ilia-itu <ilia@itu-net.com>
2024-12-26 14:58:24 +0400

    fix: update Tailwind config to use ES modules and set Node.js version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -23,8 +25,6 @@ export default {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [forms],
 }
 
